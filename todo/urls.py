982bbf9cf1c6e007b63dc817c2120f904cb3e929
Foreigner444 <testingu259@gmail.com>
2025-10-26 18:27:17 +0200

    Completed

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('add_task/', views.add_task, name='add_task'),
-    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done')
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('unmark_completed/<int:pk>/', views.unmark_completed, name='unmark_completed'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -14,3 +14,27 @@ def mark_as_done(request, pk):
     task.is_completed = True
     task.save()
     return redirect('home')
+
+def unmark_completed(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        updated_task = request.POST['task']
+        task.title = updated_task
+        task.save()
+        return redirect('home')
+    else:
+        context = {'task': task}
+        return render(request, 'edit_task.html', context)
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
